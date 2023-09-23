e44394876570ebba8f8cd2ecead15bd903e109b2
RouryR <Rouryffx@hotmail.com>
2023-09-23 17:27:16 -0600

    We added some examples
    Clase 2

---
+++ Tienda_Diego/src/main/java/Tienda_Diego/demo/TiendaDiegoApplication.java
@@ -0,0 +1,13 @@
+package Tienda_Diego.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDiegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDiegoApplication.class, args);
+	}
+
+}
