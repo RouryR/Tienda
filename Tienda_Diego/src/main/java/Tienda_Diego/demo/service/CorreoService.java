dc8db115cffbee361a0ca6527d20fee6c11bb278
RAMIREZ CORRALES DIEGO JOSE <92670091+RouryR@users.noreply.github.com>
2023-11-26 22:46:28 -0600

    Semana 11
    Se realizó la actualizcion de semana 11

---
+++ Tienda_Diego/src/main/java/Tienda_Diego/demo/service/CorreoService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tienda_Diego.demo.service;
+
+import jakarta.mail.MessagingException;
+
+/**
+ *
+ * @author ROURY
+ */
+
+public interface CorreoService {
+    public void enviarCorreoHtml(
+            String para, 
+            String asunto, 
+            String contenidoHtml) 
+            throws MessagingException;
+}
